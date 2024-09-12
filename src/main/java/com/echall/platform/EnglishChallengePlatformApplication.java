e5c32d06445c42b9af1d15cf5a5097784862aa29
xorwns118 <xorwns118@naver.com>
2024-09-12 16:19:30 +0900

    test

--- src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
+++ src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnglishChallengePlatformApplication {
-
+	//test
 	public static void main(String[] args) {
 		SpringApplication.run(EnglishChallengePlatformApplication.class, args);
 	}
