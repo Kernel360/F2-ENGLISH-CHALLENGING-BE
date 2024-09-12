35d7b2ceb289840b5c728aab9c4ecd8bb38f5ab1
Tigris <kyrae604@naver.com>
2024-09-12 13:27:07 +0900

    [FEAT] : Default Setting

---
+++ src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.echall.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishChallengePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishChallengePlatformApplication.class, args);
+	}
+
+}
