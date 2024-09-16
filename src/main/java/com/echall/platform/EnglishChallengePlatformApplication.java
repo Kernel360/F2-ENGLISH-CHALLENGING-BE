d83ecc085e92ce2a482654cbccca9c4638636f33
Minju-Kimm <alswn299842@naver.com>
2024-09-17 02:20:45 +0900

    [feat] 환경 설정 파일 수정 및 애플리케이션 설정 변경

--- src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
+++ src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
@@ -2,8 +2,14 @@ package com.echall.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.echall.platform.user.repository")
+@EnableMongoRepositories(basePackages = "com.echall.platform.content.repository")
 public class EnglishChallengePlatformApplication {
 
 	public static void main(String[] args) {
