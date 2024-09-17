af2886b550201f4b344d2bb990b53f3e90a1d621
Minju-Kimm <alswn299842@naver.com>
2024-09-17 16:31:28 +0900

    [feat] 카테고리 리포지토리 추가 #16

--- src/main/java/com/echall/platform/category/repository/CategoryRepository.java
+++ src/main/java/com/echall/platform/category/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.echall.platform.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.echall.platform.category.domain.entity.CategoryEntity;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
 }
