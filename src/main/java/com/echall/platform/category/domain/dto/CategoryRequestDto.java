ec263884a897d83922fb7407fe470186b1501fc9
Minju-Kimm <alswn299842@naver.com>
2024-09-17 16:31:28 +0900

    [feat] 카테고리 요청 DTO 추가 #16

--- src/main/java/com/echall/platform/category/domain/dto/CategoryRequestDto.java
+++ src/main/java/com/echall/platform/category/domain/dto/CategoryRequestDto.java
@@ -1,4 +1,13 @@
 package com.echall.platform.category.domain.dto;
 
-public record CategoryRequestDto() {
+import com.echall.platform.category.domain.entity.CategoryEntity;
+
+public record CategoryRequestDto(
+	String name
+) {
+	public CategoryEntity toEntity() {
+		return CategoryEntity.builder()
+			.name(this.name)
+			.build();
+	}
 }
