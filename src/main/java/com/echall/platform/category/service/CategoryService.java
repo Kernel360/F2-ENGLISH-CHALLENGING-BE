cb165bea17f6326b5df4d1030a5317d75d909a6b
Minju-Kimm <alswn299842@naver.com>
2024-09-17 16:31:28 +0900

    [feat] 카테고리 서비스 인터페이스 추가 #16

--- src/main/java/com/echall/platform/category/service/CategoryService.java
+++ src/main/java/com/echall/platform/category/service/CategoryService.java
@@ -8,7 +8,6 @@ import com.echall.platform.category.domain.dto.CategoryResponseDto;
 public interface CategoryService {
 	CategoryResponseDto createCategory(CategoryRequestDto categoryRequestDto); //새로운 카테고리 생성하기
 	CategoryResponseDto updateCategory(Long id, CategoryRequestDto updatedCategory);
-	CategoryResponseDto deleteCategory(Long id);
 
 	List<CategoryResponseDto> findAllCategory(); //모든 카테고리 가져오기
 
