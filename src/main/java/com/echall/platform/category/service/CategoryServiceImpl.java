9faea00eb189bad81c365916b048995bbd154d6c
Minju-Kimm <alswn299842@naver.com>
2024-09-17 16:31:28 +0900

    [feat] 카테고리 서비스 구현체 추가 #16

--- src/main/java/com/echall/platform/category/service/CategoryServiceImpl.java
+++ src/main/java/com/echall/platform/category/service/CategoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.echall.platform.category.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.echall.platform.category.domain.dto.CategoryRequestDto;
@@ -13,23 +15,51 @@ import com.echall.platform.category.repository.CategoryRepository;
 public class CategoryServiceImpl implements CategoryService{
 
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	public CategoryServiceImpl(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
 	@Override
-	public List<CategoryEntity> getAllCategories() {
-		return categoryRepository.findAll();
+	public List<CategoryResponseDto> findAllCategory() {
+		List<CategoryEntity> categories = categoryRepository.findAll();
+
+		return categories.stream()
+			.map(CategoryResponseDto::of)
+			.toList();
+	}
+
+	@Override
+	public CategoryResponseDto findCategoryById(Long id) {
+		return CategoryResponseDto.of(categoryRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID의 카테고리를 찾을 수 없습니다.")));
 	}
 
 	@Override
 	public CategoryResponseDto createCategory(CategoryRequestDto categoryRequestDto) {
-		return null;
+
+		CategoryEntity newCategory = categoryRequestDto.toEntity();
+		CategoryEntity savedCategory = categoryRepository.save(newCategory);
+
+		return CategoryResponseDto.of(savedCategory);
 	}
 
 	@Override
-	public CategoryResponseDto updateCategory(Long id, CategoryRequestDto updatedCategory) {
-		return null;
+	public CategoryResponseDto updateCategory(Long id, CategoryRequestDto categoryUpdateRequestDto) {
+		Optional<CategoryEntity> optionalCategory = categoryRepository.findById(id);
+
+		if (optionalCategory.isPresent()) {
+			CategoryEntity category = optionalCategory.get();
+
+			return CategoryResponseDto.of(categoryRepository.save(
+				category.toBuilder()
+					.name(categoryUpdateRequestDto.name())
+					.build()
+			));
+		} else {
+			throw new IllegalArgumentException("해당 ID의 카테고리를 찾을 수 없습니다.");
+		}
 	}
 
-	@Override
-	public CategoryResponseDto deleteCategory(Long id) {
-		return null;
-	}
+
 }
