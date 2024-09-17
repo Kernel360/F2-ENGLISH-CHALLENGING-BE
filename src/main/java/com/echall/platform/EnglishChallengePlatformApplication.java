1dbc3000e48a6e98d787702d283437407296a48e
Tigris <kyrae604@naver.com>
2024-09-17 20:55:18 +0900

    [FEAT] : Reflect PR review

--- src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
+++ src/main/java/com/echall/platform/EnglishChallengePlatformApplication.java
@@ -2,8 +2,10 @@ package com.echall.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EnglishChallengePlatformApplication {
 
 	public static void main(String[] args) {
