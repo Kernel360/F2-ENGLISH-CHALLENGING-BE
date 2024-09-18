e44e854377aa531c6983d974843daa4228589dc6
Minju-Kimm <alswn299842@naver.com>
2024-09-18 16:43:26 +0900

    [fix] 커스텀 예외처리 - refreshtoken

--- src/main/java/com/echall/platform/oauth2/service/RefreshTokenService.java
+++ src/main/java/com/echall/platform/oauth2/service/RefreshTokenService.java
@@ -2,9 +2,7 @@ package com.echall.platform.oauth2.service;
 
 import org.springframework.stereotype.Service;
 
-import com.echall.platform.exception.common.ApiErrorCategory;
-import com.echall.platform.exception.user.ApiUserErrorSubCategory;
-import com.echall.platform.exception.user.ApiUserException;
+import com.echall.platform.exception.user.oauth2.RefreshTokenNotFoundException;
 import com.echall.platform.oauth2.domain.entity.RefreshToken;
 import com.echall.platform.oauth2.repository.RefreshTokenRepository;
 
@@ -18,10 +16,7 @@ public class RefreshTokenService {
 	public RefreshToken getRefreshToken(String refreshToken) {
 		return refreshTokenRepository.findByRefreshToken(refreshToken)
 			.orElseThrow(
-				() -> ApiUserException.builder()
-					.category(ApiErrorCategory.RESOURCE_UNAUTHORIZED)
-					.subCategory(ApiUserErrorSubCategory.USER_REFRESH_TOKEN_NOT_FOUND)
-					.build()
+				() -> new RefreshTokenNotFoundException(refreshToken)
 			);
 	}
 
