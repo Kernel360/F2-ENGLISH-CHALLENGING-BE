8fafbde60e90332a32a4d65ff5b30cff0a8ccc7f
Tigris <kyrae604@naver.com>
2024-09-30 20:22:57 +0900

    [REFACTOR] SecurityConfig 수정

--- src/main/java/com/echall/platform/config/SecurityConfig.java
+++ src/main/java/com/echall/platform/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.echall.platform.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -120,23 +122,23 @@ public class SecurityConfig {
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.addAllowedOriginPattern("*");
-		corsConfiguration.addExposedHeader("Authorization");
-		corsConfiguration.addExposedHeader("refresh_token");
-		corsConfiguration.addExposedHeader("Set-Cookie");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedMethod("*");
+		return request -> {
+			CorsConfiguration corsConfiguration = new CorsConfiguration();
+			corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+			corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+			corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000"));
+			corsConfiguration.setAllowCredentials(true);
+			return corsConfiguration;
+		};
+
 		// corsConfiguration.addAllowedOrigin(System.getenv("WEBSITE_DOMAIN"));
 		// corsConfiguration.addAllowedOrigin(System.getenv("API_DOMAIN"));
-		corsConfiguration.addAllowedOrigin("http://localhost:8080"); // JUST FOR LOCAL DEV
-		corsConfiguration.addAllowedOrigin("http://localhost:8081"); // JUST FOR LOCAL DEV
-		corsConfiguration.addAllowedOrigin("http://localhost:3000"); // JUST FOR LOCAL DEV
-		corsConfiguration.addAllowedHeader("http://13.238.253.88:8080"); // JUST FOR PROD
-		corsConfiguration.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
-		return source;
+		// corsConfiguration.addAllowedOrigin("http://localhost:8080"); // JUST FOR LOCAL DEV
+		// corsConfiguration.addAllowedOrigin("http://localhost:3000"); // JUST FOR LOCAL DEV
+		// corsConfiguration.addAllowedHeader("http://13.238.253.88:8080"); // JUST FOR PROD
+		//
+		// UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		// source.registerCorsConfiguration("/**", corsConfiguration);
+		// return source;
 	}
 }
