b4cde10d06c4cc3903cda8382743689d8f3d29f9
Tigris <kyrae604@naver.com>
2024-10-02 13:57:16 +0900

    [REFACTOR] 현재 사용하지 않는 CrawlingServiceStatus 의 응답 코드 삭제

--- src/main/java/com/echall/platform/message/status/CrawlingServiceStatus.java
+++ src/main/java/com/echall/platform/message/status/CrawlingServiceStatus.java
@@ -5,14 +5,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CrawlingServiceStatus implements ServiceStatus{
 	// success
-	CRAWLING_YOUTUBE_SUCCESS("U-CR-001"),
-	CRAWLING_CNN_SUCCESS("U-CR-002"),
 
 	// error,
-	CRAWLING_YOUTUBE_FAILURE("U-CR-901"),
-	CRAWLING_CNN_FAILURE("U-CR-902"),
-	CRAWLING_OUT_OF_BOUNDS("U-CR-903"),
-	CRAWLING_SELENIUM_FAILURE("U-CR-904")
+	CRAWLING_OUT_OF_BOUNDS("U-CR-901"),
+	CRAWLING_SELENIUM_FAILURE("U-CR-902"),
 
 
 	;
