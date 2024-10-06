bc4b18859e5fe19a9e6ac249447d65b7927b3ef1
mooncw <dusrktl4@gmail.com>
2024-10-06 18:54:29 +0900

    faet: UserDetails와 OAuth2User를 묶어주는 OAuth2UserPrincipal 추가
    - getAuthorities에 대한 로직은 추후 추가될 수 있음

---
+++ src/main/java/com/echall/platform/oauth2/domain/info/OAuth2UserPrincipal.java
@@ -0,0 +1,68 @@
+package com.echall.platform.oauth2.domain.info;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class OAuth2UserPrincipal implements UserDetails, OAuth2User {
+    private final OAuth2UserInfo oAuth2UserInfo;
+
+    // OAuth2User Override Method
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2UserInfo.getAttributes();
+    }
+
+    // UserDetails Override Method
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return oAuth2UserInfo.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
