25b8f00312c784fd68c83e751902e40e5c423258
mooncw <dusrktl4@gmail.com>
2024-10-07 12:11:50 +0900

    Refactor: TokenAuthenticationFilter 토큰 관련 에러 응답을 컨벤션에 맞게 보내도록 수정

--- src/main/java/com/echall/platform/config/TokenAuthenticationFilter.java
+++ src/main/java/com/echall/platform/config/TokenAuthenticationFilter.java
@@ -1,23 +1,26 @@
 package com.echall.platform.config;
 
+import com.echall.platform.message.error.exception.CommonException;
 import com.echall.platform.oauth2.TokenProvider;
 import com.echall.platform.util.CookieUtil;
-import groovy.util.logging.Slf4j;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Map;
 
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 	private final TokenProvider tokenProvider;
 
@@ -27,26 +30,44 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 		HttpServletResponse response,
 		FilterChain filterChain
 	) throws ServletException, IOException {
+		try {
+			Cookie[] cookies = request.getCookies();
 
-		Cookie[] cookies = request.getCookies();
-		if (cookies == null) {
+			if (cookies == null) {
+				filterChain.doFilter(request, response);
+				return;
+			}
+
+			Arrays.stream(cookies)
+				.filter(cookie -> cookie.getName().equals(CookieUtil.ACCESS_TOKEN_NAME))
+				.findFirst()
+				.ifPresent(cookie -> {
+						String token = cookie.getValue();
+						if(tokenProvider.validateToken(token)){
+							Authentication authentication = tokenProvider.getAuthentication(token);
+							SecurityContextHolder.getContext().setAuthentication(authentication);
+						}
+				});
+
 			filterChain.doFilter(request, response);
-			return;
+
+		} catch (CommonException e) {
+			setErrorResponse(response, e);
 		}
-		Arrays.stream(cookies)
-			.filter(cookie -> cookie.getName().equals(CookieUtil.ACCESS_TOKEN_NAME))
-			.findFirst()
-			.ifPresent(cookie -> {
-				try {
-				String token = cookie.getValue();
-					if(tokenProvider.validateToken(token)){
-						Authentication authentication = tokenProvider.getAuthentication(token);
-						SecurityContextHolder.getContext().setAuthentication(authentication);
-					}
-				} catch (Exception e) {
-					logger.error("Failed to validate token", e);
-				}
-			});
-		filterChain.doFilter(request, response);
+	}
+
+	private void setErrorResponse(HttpServletResponse response, CommonException e) throws IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(e.getErrorCode().getStatus().value());
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		String jsonResponse = objectMapper.writeValueAsString(
+			Map.of("code", e.getErrorCode().getCode(),
+				"message", e.getErrorCode().getMessage())
+		);
+
+		response.getWriter().write(jsonResponse);
+		response.getWriter().flush();
 	}
 }
