c77e2a823d5ccaed78c0a693efe897ef16a6e3dd
mooncw <dusrktl4@gmail.com>
2024-10-07 11:29:22 +0900

    Refactor: 소셜 로그인 시 발생하는 CommonException에 대해 응답 컨벤션에 맞게 응답을 주도록 수정
    - 나머지 Exception에 대한 응답 컨벤션에 따라 추가될 수 있음 ex) Server Internal Error

--- src/main/java/com/echall/platform/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/echall/platform/oauth2/OAuth2SuccessHandler.java
@@ -1,10 +1,13 @@
 package com.echall.platform.oauth2;
 
+import com.echall.platform.message.ResponseEntityFactory;
+import com.echall.platform.message.error.exception.CommonException;
 import com.echall.platform.oauth2.domain.info.OAuth2UserPrincipal;
 import com.echall.platform.oauth2.service.RefreshTokenService;
 import com.echall.platform.user.domain.entity.UserEntity;
 import com.echall.platform.user.service.UserService;
 import com.echall.platform.util.CookieUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -30,18 +34,36 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 	public void onAuthenticationSuccess(
 		HttpServletRequest request, HttpServletResponse response, Authentication authentication
 	) throws IOException {
-		OAuth2UserPrincipal oAuth2UserPrincipal = (OAuth2UserPrincipal) authentication.getPrincipal();
+		try {
+			OAuth2UserPrincipal oAuth2UserPrincipal = (OAuth2UserPrincipal) authentication.getPrincipal();
 
-		UserEntity user = userService.getUserByOAuthUser(oAuth2UserPrincipal);
+			UserEntity user = userService.getUserByOAuthUser(oAuth2UserPrincipal);
 
-		String refreshToken = tokenProvider.generateRefreshToken(user);
-		cookieUtil.addRefreshTokenCookie(request, response, refreshToken);
+			String refreshToken = tokenProvider.generateRefreshToken(user);
+			cookieUtil.addRefreshTokenCookie(request, response, refreshToken);
 
-		String accessToken = tokenProvider.generateAccessToken(user);
-		cookieUtil.addAccessTokenCookie(request, response, accessToken);
+			String accessToken = tokenProvider.generateAccessToken(user);
+			cookieUtil.addAccessTokenCookie(request, response, accessToken);
 
-		refreshTokenService.saveRefreshToken(user, refreshToken);
+			refreshTokenService.saveRefreshToken(user, refreshToken);
 
-		response.sendRedirect(oAuth2SuccessRedirectUri);
+			response.sendRedirect(oAuth2SuccessRedirectUri);
+
+		} catch (CommonException e) {
+			response.setContentType("application/json;charset=UTF-8");
+			response.setStatus(e.getErrorCode().getStatus().value());
+
+			ObjectMapper objectMapper = new ObjectMapper();
+
+			String jsonResponse = objectMapper.writeValueAsString(
+				Map.of("code", e.getErrorCode().getCode(),
+					"message", e.getErrorCode().getMessage())
+			);
+
+			response.getWriter().write(jsonResponse);
+			response.getWriter().flush();
+		}
+
+		// TODO: 나머지 Exception에 대한 응답 컨벤션에 따라 추가될 수 있음 ex) Server Internal Error
 	}
 }
