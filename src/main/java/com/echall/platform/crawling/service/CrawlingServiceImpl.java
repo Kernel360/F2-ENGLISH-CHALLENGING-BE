c1fe643878d0343ca34ba9c5980611865c778040
Tigris <kyrae604@naver.com>
2024-10-11 12:53:42 +0900

    [HOTFIX] headless 다시 추가

--- src/main/java/com/echall/platform/crawling/service/CrawlingServiceImpl.java
+++ src/main/java/com/echall/platform/crawling/service/CrawlingServiceImpl.java
@@ -136,12 +136,14 @@ public class CrawlingServiceImpl implements CrawlingService {
 		options.addArguments("--lang=en-US");
 		options.addArguments("--start-maximized");
 		// options.addArguments("--window-size=1920,1080");
+		options.addArguments("--headless");
 
 		WebDriverManager.chromedriver().setup();
 
 		if (os.contains("linux")) {
 			log.error("LINUX");
 			// Ubuntu의 경우
+			options.addArguments("--start-maximized");
 			options.addArguments("--headless");
 			options.addArguments("--no-sandbox");
 			options.addArguments("--disable-dev-shm-usage");
