7bbdd5421512b186b8a03a2573a59ca5aac95479
mooncw <dusrktl4@gmail.com>
2024-10-11 14:20:35 +0900

    Refactor: ContentPublicController의 getContents 메서드 분리
    - 리딩, 리스닝으로 분리

--- src/main/java/com/echall/platform/content/controller/ContentPublicController.java
+++ src/main/java/com/echall/platform/content/controller/ContentPublicController.java
@@ -1,28 +1,12 @@
 package com.echall.platform.content.controller;
 
-import static com.echall.platform.message.response.ContentResponseCode.*;
-
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.echall.platform.content.domain.dto.ContentPageResponse;
 import com.echall.platform.content.domain.dto.ContentResponseDto;
 import com.echall.platform.content.domain.enums.ContentType;
-import com.echall.platform.content.domain.enums.SearchCondition;
 import com.echall.platform.content.service.ContentService;
 import com.echall.platform.message.ApiCustomResponse;
 import com.echall.platform.message.ResponseEntityFactory;
-
+import com.echall.platform.util.PaginationDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -33,6 +17,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.echall.platform.message.response.ContentResponseCode.CONTENT_VIEW_SUCCESS;
 
 @RequiredArgsConstructor
 @RestController
@@ -46,8 +39,8 @@ public class ContentPublicController {
 	 * 컨텐츠 조회
 	 * (pageable)
 	 */
-	@GetMapping("/view")
-	@Operation(summary = "컨텐츠 조회", description = "페이지네이션을 적용하여 전체 컨텐츠 목록을 조회합니다.")
+	@GetMapping("/view/reading")
+	@Operation(summary = "리딩 컨텐츠 조회", description = "페이지네이션을 적용하여 리딩 컨텐츠 목록을 조회합니다.")
 	@ApiResponses(value = {
 		@ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = {
 			@Content(mediaType = "application/json", schema = @Schema(implementation = ContentPageResponse.class))
@@ -56,16 +49,40 @@ public class ContentPublicController {
 		@ApiResponse(responseCode = "500", description = "서버 에러가 발생하였습니다.", content = @Content)
 	})
 	@Parameters({
-		@Parameter(name = "page", description = "페이지 번호 (0부터 시작)", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "0")),
-		@Parameter(name = "size", description = "페이지당 데이터 수", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "10")),
-		@Parameter(name = "sort", description = "정렬 기준 (예: createdAt,desc)", in = ParameterIn.QUERY, schema = @Schema(type = "string"))
+		@Parameter(name = "page", description = "페이지 번호 (0부터 시작) / default: 0", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "0")),
+		@Parameter(name = "size", description = "페이지당 데이터 수 / default: 10", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "10")),
+		@Parameter(name = "sort", description = "정렬 기준 (createdAt, hits) / default: createdAt", in = ParameterIn.QUERY, schema = @Schema(type = "string")),
+		@Parameter(name = "direction", description = "정렬 방법 / default: desc", in = ParameterIn.QUERY, schema = @Schema(type = "string"))
 	})
-	public ResponseEntity<ApiCustomResponse<Page<ContentResponseDto.ContentViewResponseDto>>> getContents(
-		@Parameter(hidden = true) @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-		SearchCondition searchCondition
+	public ResponseEntity<ApiCustomResponse<PaginationDto<ContentResponseDto.ContentPreviewResponseDto>>> getReadingContents(
+		@Parameter(hidden = true) @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
 	) {
-		Page<ContentResponseDto.ContentViewResponseDto> pageContentList
-			= contentService.getAllContents(pageable, searchCondition);
+		PaginationDto<ContentResponseDto.ContentPreviewResponseDto> pageContentList
+			= contentService.getAllContents(ContentType.READING, pageable);
+
+		return ResponseEntityFactory.toResponseEntity(CONTENT_VIEW_SUCCESS, pageContentList);
+	}
+
+	@GetMapping("/view/listening")
+	@Operation(summary = "리스닝 컨텐츠 조회", description = "페이지네이션을 적용하여 리스닝 컨텐츠 목록을 조회합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ContentPageResponse.class))
+		}),
+		@ApiResponse(responseCode = "204", description = "컨텐츠가 없습니다.", content = @Content),
+		@ApiResponse(responseCode = "500", description = "서버 에러가 발생하였습니다.", content = @Content)
+	})
+	@Parameters({
+		@Parameter(name = "page", description = "페이지 번호 (0부터 시작) / default: 0", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "0")),
+		@Parameter(name = "size", description = "페이지당 데이터 수 / default: 10", in = ParameterIn.QUERY, schema = @Schema(type = "integer", defaultValue = "10")),
+		@Parameter(name = "sort", description = "정렬 기준 (createdAt, hits) / default: createdAt", in = ParameterIn.QUERY, schema = @Schema(type = "string")),
+		@Parameter(name = "direction", description = "정렬 방법 / default: desc", in = ParameterIn.QUERY, schema = @Schema(type = "string"))
+	})
+	public ResponseEntity<ApiCustomResponse<PaginationDto<ContentResponseDto.ContentPreviewResponseDto>>> getListeningContents(
+		@Parameter(hidden = true) @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
+	) {
+		PaginationDto<ContentResponseDto.ContentPreviewResponseDto> pageContentList
+			= contentService.getAllContents(ContentType.LISTENING, pageable);
 
 		return ResponseEntityFactory.toResponseEntity(CONTENT_VIEW_SUCCESS, pageContentList);
 	}
