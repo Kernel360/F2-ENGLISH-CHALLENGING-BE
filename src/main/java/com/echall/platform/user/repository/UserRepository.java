37e39374526a21a8c822a0bb5e0894736adb8bc4
Tigris <kyrae604@naver.com>
2024-10-13 15:22:12 +0900

    [FEAT] 북마크 전체 조회 기능 추가

--- src/main/java/com/echall/platform/user/repository/UserRepository.java
+++ src/main/java/com/echall/platform/user/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.echall.platform.user.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.echall.platform.bookmark.domain.entity.BookmarkEntity;
 import com.echall.platform.user.domain.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
