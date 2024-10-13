54e397e5631fee79c7427fb4680fad11a9132f7a
Tigris <kyrae604@naver.com>
2024-10-13 15:21:37 +0900

    [FEAT] 북마크 전체 조회 기능 추가

--- src/main/java/com/echall/platform/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/echall/platform/bookmark/repository/BookmarkRepository.java
@@ -1,7 +1,5 @@
 package com.echall.platform.bookmark.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.echall.platform.bookmark.domain.entity.BookmarkEntity;
