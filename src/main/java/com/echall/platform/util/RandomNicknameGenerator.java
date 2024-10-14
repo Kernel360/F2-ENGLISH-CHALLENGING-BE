43faeed15201a501d1f1fc4cf19ed31920c09b17
Tigris <kyrae604@naver.com>
2024-10-14 14:52:47 +0900

    [REFACTORING] 랜덤 닉네임 기본값 수정

--- src/main/java/com/echall/platform/util/RandomNicknameGenerator.java
+++ src/main/java/com/echall/platform/util/RandomNicknameGenerator.java
@@ -10,14 +10,16 @@ import lombok.Getter;
 @Component
 @Getter
 public class RandomNicknameGenerator {
-	private static final List<String> prefixNicknames = List.of(new String[] {"pre1", "pre2", "pre3"});
-	private static final List<String> suffixNicknames = List.of(new String[] {"suf1", "suf2", "suf3"});
+	private static final List<String> prefixNicknames
+		= List.of(new String[] {"빠른", "용감한", "친절한", "행복한", "빛나는"});
+	private static final List<String> suffixNicknames
+		= List.of(new String[] {"여우", "강아지", "고양이", "다람쥐", "도마뱀"});
 	private static final Random random = new Random();
 
 	public static String setRandomNickname() {
-		return prefixNicknames.get((int)(Math.random() % 3)) +
-			suffixNicknames.get((int)(Math.random() % 3)) +
-			String.format("%04d", random.nextInt(10000));
+		return prefixNicknames.get((int)(Math.random() % 5)) +
+			" " + suffixNicknames.get((int)(Math.random() % 5)) +
+			" " + String.format("%04d", random.nextInt(10000));
 	}
 
 }
