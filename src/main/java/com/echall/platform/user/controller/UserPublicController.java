9014d8ffe9b6e8da1d65365b4ecc7a51530772d3
mooncw <dusrktl4@gmail.com>
2024-10-15 10:59:23 +0900

    Refactor: 기존 UserPublicController 메서드 중 login만 남김

--- src/main/java/com/echall/platform/user/controller/UserPublicController.java
+++ src/main/java/com/echall/platform/user/controller/UserPublicController.java
@@ -1,39 +1,16 @@
 package com.echall.platform.user.controller;
 
-import com.echall.platform.util.CookieUtil;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class UserPublicController {
-	private final CookieUtil cookieUtil;
 
+	// 개발용 컨트롤러
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
-
-	@GetMapping("/signup")
-	public String join() {
-		return "signup";
-	}
-
-	@GetMapping("/logout")
-	public String logout(HttpServletRequest request, HttpServletResponse response) {
-		cookieUtil.removeAccessTokenCookie(request, response);
-
-		cookieUtil.removeRefreshTokenCookie(request, response);
-
-		new SecurityContextLogoutHandler().logout(request, response
-			, SecurityContextHolder.getContext().getAuthentication());
-
-		return "redirect:/login";
-	}
-
 }
